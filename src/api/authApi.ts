e0b0df9323e7352507cacb1ad0ed2487f1b89e7e
Jerome <jerome@ntu.im>
2025-05-02 01:20:38 +0800

    Update authApi.ts

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+/** 使用者登入 */
 export async function login(employeeCode: string, password: string) {
     const response = await axios.post('http://localhost:8080/api/auth/login', {
         employeeCode,
